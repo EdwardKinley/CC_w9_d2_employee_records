f310dc84e9c1b99889c1af30f3064792672f053d
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-17 17:01:08 +0100

    adds Employees SHOW

--- src/main/java/controllers/EmployeesController.java
+++ src/main/java/controllers/EmployeesController.java
@@ -23,13 +23,22 @@ public class EmployeesController {
         ManagersController managersController = new ManagersController();
         EngineersController engineersController = new EngineersController();
 
-        get("/employees", (res, req) -> {
+        get("/employees", (req, res) -> {
             Map<String, Object> model = new HashMap();
             model.put("template", "templates/employees/index.vtl");
 
             List<Employee> employees = DBHelper.getAll(Employee.class);
             model.put("employees", employees);
 
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/employees/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/employees/show.vtl");
+            int employeeId = Integer.parseInt(req.params(":id"));
+            Employee employee = DBHelper.find(employeeId, Employee.class);
+            model.put("employee", employee);
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
